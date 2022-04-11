fe886d1bfeb9a00d83b7f02485d9c88487bc2e25
Dup4 <lyuzhi.pan@gmail.com>
2022-04-11 23:49:56 +0800

    chore: .hpp -> .h

--- test/hello_world_benchmark.cc
+++ test/hello_world_benchmark.cc
@@ -1,6 +1,6 @@
 #include <benchmark/benchmark.h>
 
-#include "starter-cpp/hello_world.hpp"
+#include "starter-cpp/hello_world.h"
 
 static void BenchmarkHelloWorld(benchmark::State& state) {
     for (auto _ : state) {
--- test/hello_world_test.cc
+++ test/hello_world_test.cc
@@ -1,6 +1,6 @@
 #include <gtest/gtest.h>
 
-#include "starter-cpp/hello_world.hpp"
+#include "starter-cpp/hello_world.h"
 
 TEST(HELLO_WORLD, hello_world) {
     EXPECT_EQ(HelloWorld(), "Hello World");
