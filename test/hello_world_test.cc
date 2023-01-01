fb163ce58e9608e488bb56ecf5b7c9c1728fecbd
Dup4 <lyuzhi.pan@gmail.com>
2023-01-01 22:54:30 +0800

    refactor: unittest spec

--- test/hello_world_test.cc
+++ test/hello_world_test.cc
@@ -1,11 +1,13 @@
 #include "gtest/gtest.h"
 #include "snapshot/snapshot.h"
 
+#include "starter-cpp/hello_world.h"
+
 class HelloWorldTest : public testing::Test {
 protected:
     virtual void SetUp() override {}
 };
 
 TEST_F(HelloWorldTest, hello_world_test) {
-    EXPECT_EQ("Hello World", "Hello World");
+    EXPECT_EQ(HelloWorld(), "Hello World");
 }
